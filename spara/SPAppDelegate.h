cdbba13ce11e179ec3759a58406f7bb734960e4c
adexDev <adexDeveloper@gmail.com>
2013-08-08 22:08:41 +0100

    Initial Commit

---
+++ spara/SPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPAppDelegate.h
+//  spara
+//
+//  Created by Adex on 08/08/2013.
+//  Copyright (c) 2013 Spara Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ spara/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  spara
+//
+//  Created by Adex on 08/08/2013.
+//  Copyright (c) 2013 Spara Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPAppDelegate class]));
+    }
+}
